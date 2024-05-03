442d38cba889a1835221420465126182977527a6
Beka <>
2024-05-03 19:20:30 +0600

    Updates installed apps

--- source/main/urls.py
+++ source/main/urls.py
@@ -37,8 +37,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/managers/', include('managers.urls')),
-    path('api/v1/apartment/', include('apartment.urls')),
+    path('api/v1/managers/', include('source.managers.urls')),
+    path('api/v1/apartment/', include('source.apartment.urls')),
 
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
