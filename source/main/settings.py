acd7070d471792e670fc4af4c7393ece6c57be1e
Beka <>
2024-05-03 18:52:48 +0600

    Adds source

--- source/main/settings.py
+++ source/main/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'main.urls'
+ROOT_URLCONF = 'source.main.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'main.wsgi.application'
+WSGI_APPLICATION = 'source.main.wsgi.application'
 
 
 # Database
