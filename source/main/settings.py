cda6844b36aa3e27453bcbd6602c50fe5d13abdb
Beka <>
2024-05-05 00:47:53 +0600

    updates

--- source/main/settings.py
+++ source/main/settings.py
@@ -13,6 +13,8 @@ from dotenv import load_dotenv
 import dj_database_url
 import os
 from pathlib import Path
+from decouple import Config
+
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['crmconstruction.com', 'crmconstruction-a8cfcab89ef3.herokuapp.com']
 
@@ -85,22 +87,31 @@ WSGI_APPLICATION = 'source.main.wsgi.application'
 
 # settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'da62su942i51v1',
-        'USER': 'zrhqlklmqyzdvf',
-        'PASSWORD': '82a4d8ac7137b8685f0b98a6a3c31a909d1c6ac30c9028d9e499e5cca6ca270e',
-        'HOST': 'ec2-44-194-102-142.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'da62su942i51v1',
+#         'USER': 'zrhqlklmqyzdvf',
+#         'PASSWORD': '82a4d8ac7137b8685f0b98a6a3c31a909d1c6ac30c9028d9e499e5cca6ca270e',
+#         'HOST': 'ec2-44-194-102-142.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
-DATABASES['default']['CONN_MAX_AGE'] = 600
+# DATABASES['default']['CONN_MAX_AGE'] = 600
 
 # DATABASES = {
 #     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 # }
+config = Config()
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
