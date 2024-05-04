4d217590ac15e3c0dcd1efdc067d02d050b7c3af
Beka <>
2024-05-05 01:04:47 +0600

    decopule

--- source/main/settings.py
+++ source/main/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from dotenv import load_dotenv
-import dj_database_url
 import os
+from decouple import config
+# from dotenv import load_dotenv
+import dj_database_url
 from pathlib import Path
-from decouple import Config
 
 
-load_dotenv()
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,7 +103,6 @@ WSGI_APPLICATION = 'source.main.wsgi.application'
 # DATABASES = {
 #     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 # }
-config = Config()
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
