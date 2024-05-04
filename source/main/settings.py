766a2563b8c0c2b727a5e19f0bd84e4091177814
Beka <>
2024-05-04 22:58:23 +0600

    PostgreSQL via Heroku

--- source/main/settings.py
+++ source/main/settings.py
@@ -99,9 +99,7 @@ WSGI_APPLICATION = 'source.main.wsgi.application'
 # DATABASES['default']['CONN_MAX_AGE'] = 600
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
